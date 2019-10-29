becbb75f2fe32ee56a7758ab8318fe5a6aa077a4
Benjamin Kyhn <benj2605@edu.easj.dk>
2019-10-29 12:35:51 +0100

    Listing 7.6 added

---
+++ src/Listing_7_06.java
@@ -0,0 +1,21 @@
+public class Listing_7_06 {
+    public static void main(String[] args) {
+        int[] list = {1, 4, 4, 2, 5, -3, 6, 2};
+        int i = linearSearch(list, 4); // Returns 1
+        int j = linearSearch(list, -4); // Returns -1
+        int k = linearSearch(list, -3); // Returns 5
+
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(k);
+    }
+
+    /** The method for finding a key in the list */
+    public static int linearSearch(int[] list, int key){
+        for (int i = 0; i < list.length; i++) {
+            if (key == list[i])
+                return i;
+        }
+        return -1;
+    }
+}
