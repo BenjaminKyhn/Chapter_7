cfc72d35e17981804bd493529ca209081d1a1641
Benjamin Kyhn <benkyhn@hotmail.com>
2019-10-30 10:00:57 +0100

    Listing 7.7 added

--- src/Listing_7_07.java
+++ src/Listing_7_07.java
@@ -1,4 +1,19 @@
 public class Listing_7_07 {
+    public static void main(String[] args) {
+        int[] list = {2, 4, 7, 10, 11, 45, 50, 59, 60, 66, 69, 70, 79};
+        int i = binarySearch(list, 2); // Returns 0
+        int j = binarySearch(list, 11); // Returns 4
+        int k = binarySearch(list, 12); // Returns –6
+        int l = binarySearch(list, 1); // Returns –1
+        int m = binarySearch(list, 3); // Returns –2
+
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(k);
+        System.out.println(l);
+        System.out.println(m);
+    }
+
     /**
      * Use binary search to find the key in the list
      */
@@ -8,7 +23,7 @@ public class Listing_7_07 {
 
         while (high >= low) {
             int mid = (low + high) / 2;
-            if (key > list[mid])
+            if (key < list[mid])
                 high = mid - 1;
             else if (key == list[mid])
                 return mid;
