6804735b10f12253cd6c6f630ee54f67001f934e
Benjamin Kyhn <benkyhn@hotmail.com>
2019-10-31 12:11:47 +0100

    Listing 7.8 added

--- src/Listing_7_08.java
+++ src/Listing_7_08.java
@@ -1,7 +1,10 @@
+import java.util.Arrays;
+
 public class Listing_7_08 {
     public static void main(String[] args) {
         double[] list = {1, 9, 4.5, 6.6, 5.7, -4.5};
         selectionSort(list);
+        System.out.println(Arrays.toString(list));
     }
 
     /** The method for sorting the numbers */
