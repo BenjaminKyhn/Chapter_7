21112b4703491e04cf0ac8a9be0af40e85bc89b9
Benjamin Kyhn <benj2605@edu.easj.dk>
2019-11-01 12:54:47 +0100

    Exercise 7.8 added

--- src/Exercise_7_08.java
+++ src/Exercise_7_08.java
@@ -1,11 +1,23 @@
+import java.util.Scanner;
+
 public class Exercise_7_08 {
     public static void main(String[] args) {
-        int[] list1 = {0,1,2,3,4,5,6};
-        double[] list2 = {0,1,2,3,4,5,6};
-        System.out.println(average(list1));
-        System.out.println(average(list2));
+        Scanner input = new Scanner(System.in);
+
+        // Initialize an array with length 10
+        double[] numbers = new double[10];
+
+        // Fill the array with values
+        System.out.println("Enter 10 double values: ");
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = input.nextDouble();
+        }
+
+        // Invoke the average method. Java automatically knows which of the methods to use.
+        System.out.println(average(numbers));
     }
 
+    // Method for calculating the average using integer values
     public static int average(int[] array){
         int sum = 0;
         for (int i = 0; i < array.length; i++) {
@@ -14,6 +26,7 @@ public class Exercise_7_08 {
         return sum/array.length;
     }
 
+    // Method for calculating the average using double values
     public static double average(double[] array){
         double sum = 0;
         for (int i = 0; i < array.length; i++) {
