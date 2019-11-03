4c7608fa012f56096025458a9939388dc32856fe
Benjamin Kyhn <benkyhn@hotmail.com>
2019-11-03 14:33:18 +0100

    Exercise 7.12 added

--- src/Exercise_7_12.java
+++ src/Exercise_7_12.java
@@ -7,13 +7,16 @@ public class Exercise_7_12 {
         Scanner input = new Scanner(System.in);
         System.out.println("Input 10 numbers to reverse them: ");
 
+        // Fill the array with numbers
         for (int i = 0; i < array.length; i++) {
             array[i] = input.nextInt();
         }
 
+        // Print out the reversed array
         System.out.println(Arrays.toString(reverse(array)));
     }
 
+    // Method for reversing an array
     public static int[] reverse(int[] list) {
         int[] result = new int[list.length];
 
