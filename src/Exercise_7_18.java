d0e4a46ee808d6ef44152f76b185e986c9d23840
Benjamin Kyhn <benj2605@edu.easj.dk>
2019-11-05 10:41:26 +0100

    Exercise 7.18 added

---
+++ src/Exercise_7_18.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Exercise_7_18 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter 10 double values: ");
+        double[] list = new double[10];
+
+        // Fill the array with double values
+        for (int i = 0; i < list.length; i++) {
+            list[i] = input.nextDouble();
+        }
+
+        // Sort the array
+        bubbleSort(list);
+
+        // Print the sorted array
+        for (double e :
+                list) {
+            System.out.print(e + " ");
+        }
+        System.out.println();
+    }
+
+    // Method for bubble sorting
+    public static void bubbleSort(double[] list) {
+        boolean swapped;
+        double temp;
+
+        do {
+            swapped = false;
+            for (int i = 0; i < list.length - 1; i++) {
+                if (list[i] < list[i + 1]) {
+                    temp = list[i];
+                    list[i] = list[i + 1];
+                    list[i + 1] = temp;
+                    swapped = true;
+                }
+            }
+        } while (swapped);
+    }
+}
